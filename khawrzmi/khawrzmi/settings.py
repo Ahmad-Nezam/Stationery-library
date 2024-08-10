7fe64b70b75e0ae2c78f5a767fa13647fa223c45
ahmad nezam <ahmad628go@gmail.com>
2024-08-10 18:37:26 +0300

    solo project

--- khawrzmi/khawrzmi/settings.py
+++ khawrzmi/khawrzmi/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'khawrzmi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'khawrzmi',
+        'USER' : 'root',
+        'PASSWORD' : '12345678',
+        'PORT' : 3306 ,
+        'HOST' : '127.0.0.1', 
     }
 }
 
@@ -120,7 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static'] 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
